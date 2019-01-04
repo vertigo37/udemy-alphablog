ab70dcb65f08495786b7da33e04d737f04028e2a
Vertigo37 <michael@menfashion.com>
2019-01-04 15:40:04 -0500

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
     @articles = Article.all #Notice how we added an s to article to create a new instance varia that contains all articles
@@ -9,8 +10,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
-  def edit
-    @article = Article.find(params[:id])
+  def edit 
   end
   
   
@@ -25,7 +25,6 @@ class ArticlesController < ApplicationController
   end 
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was succesfully updated"
       redirect_to article_path(@article)
@@ -36,17 +35,20 @@ class ArticlesController < ApplicationController
     
     
   def show
-    @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was succesfully deleted'
     redirect_to articles_path
   end
   
   private
+  
+  def set_article
+   @article = Article.find(params[:id])    
+  end
+  
   
   def article_params
     params.require(:article).permit(:title, :description)
