b00cf28a88f87459ae24168e23e46ab15f713c72
Vertigo37 <michael@menfashion.com>
2019-01-10 18:33:14 -0500

    associations + debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,6 @@ class ArticlesController < ApplicationController
   
   
   def create
-    debugger
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
