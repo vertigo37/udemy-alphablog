905011e42f23ad842608512f705ce7a847e0c3db
Vertigo37 <michael@menfashion.com>
2019-01-15 16:52:38 -0500

    Complete auithentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+    
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5) #Notice how we added an s to article to create a new instance varia that contains all articles
   end
@@ -54,5 +56,11 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
-   
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ get 'about', to: 'pages#about'
   get 'signup' , to: 'users#new'
 #   post 'users', to: 'users#create' or do the following:
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new' # "session" controller, method "new"
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 #   ressource create routes for all methods included in users_controller, except for the method new
 #   which has a special path create with the get 'signup' , to 'user#new' line above
