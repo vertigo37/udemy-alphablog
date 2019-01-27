db60465997887e1e13fe17cb59be9eac8094aecd
Vertigo37 <michael@menfashion.com>
2019-01-26 20:19:25 -0500

    More testing

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+
+  
   
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
    end
   
     test "should get categories index" do
@@ -12,6 +15,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
     
   test "should get new" do
+    sign_in_as(@user, "password")
     get new_category_url
     assert_response :success
   end
@@ -21,4 +25,11 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+    test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+    post categories_path, params:{category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path    
+    end
+
 end
